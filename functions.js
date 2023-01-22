bc9558684f7ada0692b07daf12caaaf32768ecb7
JJ-Bas <jef.j.bas@gmail.com>
2023-01-22 14:07:00 +0100

    functions.js afgerond

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Goedemorgen"
 
 function morningGreeter() {
-  return "Goedemorgen";
+    return "Goedemorgen";
 }
 
 const greeting = morningGreeter();
@@ -14,20 +14,57 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+    return "Novi Hogescool"
+}
 
+const school = getSchool();
+
+console.log(school);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment() {
+    return "Lekker bezig met die functies, Jef!"
+}
 
+const compl = compliment();
+
+console.log(compl)
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+    return [8, 8.5, 6, 7];
+}
 
+const grade = getGrades();
+
+console.log(grade);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+    const studentProfile = {firsname: 'Jef', lastName: 'Bas'};
+    return studentProfile
+}
+
+const profile = getDetails()
+
+console.log(profile)
+
+//test
+
+function getDetails2() {
+    return {firsname: 'Jef!', lastName: 'Bas!'};
+}
+
+const profile2 = getDetails2()
+
+console.log(profile2)
